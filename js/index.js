8faa86b067f6abfd2d1ba9c88840e2ed93986e50
Steffen Anslinger <steffen.anslinger@hotmail.com>
2023-11-30 16:09:46 +0100

    added try...catch to display error message

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 console.clear();
 
 const userElement = document.querySelector(".user");
+const errorMessage = document.querySelector(".error");
 
 async function getUser(url) {
   const response = await fetch(url);
@@ -10,10 +11,16 @@ async function getUser(url) {
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
-    const user = await getUser(event.target.dataset.url);
-    userElement.innerHTML = `
+    try {
+      const user = await getUser(event.target.dataset.url);
+      userElement.innerHTML = `
     <h2>${user.first_name} ${user.last_name}</h2>
     <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
     `;
+      errorMessage.textContent = "";
+    } catch (error) {
+      userElement.innerHTML = "";
+      errorMessage.textContent = "Could not be found - please check URL";
+    }
   })
 );
